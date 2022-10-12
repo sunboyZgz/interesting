f0d9961835145b58070cad3d859e6f80b79001d7
sunboyZgz <zhugezhen.cn@aliyun.com>
2022-10-12 10:34:52 +0800

    float expression: implement a easy expression

--- src/components/009.vue
+++ src/components/009.vue
@@ -2,30 +2,33 @@
  * @Author: sunboy
  * @LastEditors: sunboy
  * @Date: 2022-10-11 21:41:19
- * @LastEditTime: 2022-10-11 22:25:53
+ * @LastEditTime: 2022-10-12 10:30:25
 -->
 <template>
   <div class="w-[600px] h-[600px]" style="border: 1px black solid">
     <div class="mt-2 mb-2 min-h-2">
-      <span class="mr-1 ml-1" v-for="bit in float_format">{{ bit }}</span>
+      <span
+        class="mr-1 ml-1"
+        :class="{
+          'text-red-500': index == 0,
+          'text-blue-500': index != 0 && index < 9,
+          'text-yellow-500': index > 8,
+        }"
+        v-for="(bit, index) in float_format"
+        >{{ bit }}</span
+      >
     </div>
-    <input id="ip" @change="onChange" placeholder="1.1" />
+    <input id="ip" @change="onChange" placeholder="input a float"  />
   </div>
 </template>
 
 <script setup lang="ts">
-import { fractionAndInteger, floatFormat, floatTuple } from "./float";
-import { onMounted, ref } from "vue";
-let value: floatTuple = [0, 0];
-const float_format = ref();
+import { floatFormat } from "./float";
+import { ref } from "vue";
+const float_format = ref(new Array(32).fill(0));
 function onChange(ev: Event) {
   const v = (ev.target as HTMLInputElement).value;
-  value = fractionAndInteger(v);
-  console.log(value);
-  float_format.value = floatFormat(v.startsWith("-"), value);
+  float_format.value = floatFormat(v);
+  console.log("float_format: ", float_format.value.join(""));
 }
-onMounted(() => {
-  const input = document.getElementById("ip") as HTMLInputElement;
-  float_format.value = floatFormat(input.value.startsWith("-"), value);
-});
 </script>
--- src/components/float.ts
+++ src/components/float.ts
@@ -2,30 +2,82 @@
  * @Author: sunboy
  * @LastEditors: sunboy
  * @Date: 2022-10-11 21:48:02
- * @LastEditTime: 2022-10-11 22:38:35
+ * @LastEditTime: 2022-10-12 10:23:28
  */
-export type floatTuple = [integer: string, fraction: string];
-function fractionAndInteger(value: string): floatTuple {
-  if (!value) {
-    console.warn("no value be passed");
-    return ["", ""];
-  }
-  return value.split(".").slice(0, 2) as floatTuple;
-}
+
+// const DOUBLE_E_BIASED = 2 ** 10 - 1;
+const FLOAT_E_BIASED = 2 ** 7 - 1;
 
 /**
+ * @description there is a diverse expression between float and integer,
+ * the fundamental reason is the difference between signifying expression and complement expression
  * @param flag indicate that it is a Positive  if flag == true, or it is a Negative
  */
-function floatFormat(flag: boolean, float: floatTuple): number[] {
+function floatFormat(float: string, lord: "f32" | "f64" = "f32"): number[] {
   const result = new Array(32).fill(0); //a 32 bits formats
-  if (!flag) {
+  let num_float = parseFloat(float);
+  if (num_float == 0) {
+    return result;
+  }
+  if (num_float < 0) {
     result[0] = 1;
   }
-  let [is, fs] = float;
-  if (is == "0") {
-    is = "1";
-    // fs = fs.
+  if (float.indexOf(".") == -1) {
+    float = float + ".0";
   }
+  //during operating the string, we divide the points and integer into two different groups
+  //by this way, we could get a precision result without the binary multiply and divide
+  const integer =
+    parseInt(
+      num_float < 0
+        ? float.slice(1, float.indexOf("."))
+        : float.slice(0, float.indexOf("."))
+    ) || 0;
+  const point = parseFloat("0" + float.slice(float.indexOf(".")));
+  let integer_bits = integer.toString(2).split("");
+  let point_bits = point.toString(2).slice(2).split("");
+  let po = 0;
+  if (integer != 0) {
+    po += integer_bits.length - 1;
+    integer_bits = integer_bits.slice(1);
+  } else {
+    for (let i = 0; i < point_bits.length; i++) {
+      po--;
+      if (parseInt(point_bits[i]) == 1) {
+        point_bits = point_bits.slice(i + 1);
+        break;
+      }
+    }
+  }
+  let tail_bits: string[] = (
+    integer == 0 ? point_bits : integer_bits.concat(point_bits)
+  ).slice(0, 23);
+  let exponent = po + FLOAT_E_BIASED;
+  console.log("integer_bits: ", integer_bits);
+  console.log("tail_bits: ", tail_bits);
+  console.log("float: ", float);
+  console.log("po: ", po);
+  console.log("exponent: ", exponent);
+  const exponent_section = exponent.toString(2).split("");
+  console.log("exponent_section: ", exponent_section);
+  fill_float(
+    result,
+    exponent_section,
+    9 - exponent_section.length,
+    exponent_section.length
+  );
+  fill_float(result, tail_bits, 9, tail_bits.length);
   return result;
 }
-export { fractionAndInteger, floatFormat };
+
+function fill_float(
+  target: string[],
+  source: string[],
+  start: number,
+  n: number
+) {
+  for (let i = start, j = 0; i < start + n; i++, j++) {
+    target[i] = source[j];
+  }
+}
+export { floatFormat };
